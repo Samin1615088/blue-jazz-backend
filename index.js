1de2f85024b07e0d5a0fbdfaf48dbe28bb2c77df
Samin1615088 <samin.shafkat@gmail.com>
2021-04-17 10:03:22 +0600

    previous commit was not working. now server is working and  accepting req for allServices and allTestimonials

--- index.js
+++ index.js
@@ -1,18 +1,61 @@
-const express = require('express');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// require('dotenv').config();
+// const MongoClient = require('mongodb').MongoClient;
+// const ObjectId = require('mongodb').ObjectId;
+
+
+// const app = express();
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const port = process.env.PORT || 5000;
+// const password = '';
+// //mongodb connection
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emkeh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+// //mongodb >> >>
+// client.connect(err => {
+//     const servicesCollection = client.db("blueJazzDb").collection("allServices");
+//     console.log("mongodb connected successfully");
+
+//     // app.get('/allServices', (req, res) => {
+//     //     console.log("req called", req);
+//     //     res.send(true)
+//     // })
+
+// });
+// //mongodb << <<
+
+
+// app.listen(() => {
+//     console.log(port);
+//     console.log(`Example app listening at http://localhost:${port}`);
+// })
+
+
+
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
+const express = require('express')
 
 
-const app = express();
+const app = express()
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = 3000
-const password = '';
+const port = process.env.PORT || 5000
 //mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emkeh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -21,17 +64,40 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-//mongodb >> >>
+
+//mongodb >> >> >> >>
 client.connect(err => {
-    const servicesCollection = client.db("blueJazzDb").collection("services");
+    const servicesCollection = client.db("blueJazzDb").collection("allServices");
+    const customerTestimonialsCollection = client.db("blueJazzDb").collection("customerTestimonials");
     console.log("mongodb connected successfully");
-    client.close();
+
+    // get all Services from mongodb >>>> 
+    app.get('/allServices', (req, res) => {
+        console.log("getALLSERVICES req", req);
+        servicesCollection.find({})
+            .toArray((err, documents) => {
+                console.log(err, documents);
+                res.send(documents);
+            })
+    })
+    // get all Services from mongodb <<<<
+
+    // get all Services from mongodb >>>> 
+    app.get('/allTestimonials', (req, res) => {
+        console.log("getALLTESTIMONIALS req", req);
+        customerTestimonialsCollection.find({})
+            .toArray((err, documents) => {
+                console.log(err, documents);
+                res.send(documents);
+            })
+    })
+    // get all Services from mongodb <<<<
 
 });
-//mongodb << <<
+//mongodb << << << <<
 
 
-app.listen(() => {
-    const currentPort = process.env.PORT || 5000;
-    console.log(`Example app listening at http://localhost:${currentPort}`)
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 })
