f478b5e75786a6209ce419f274ef9d18d2143353
Samin1615088 <samin.shafkat@gmail.com>
2021-04-18 18:17:19 +0600

    not satisfied

--- index.js
+++ index.js
@@ -1,59 +1,17 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// require('dotenv').config();
-// const MongoClient = require('mongodb').MongoClient;
-// const ObjectId = require('mongodb').ObjectId;
-
-
-// const app = express();
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// const port = process.env.PORT || 5000;
-// const password = '';
-// //mongodb connection
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emkeh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
-
-// //mongodb >> >>
-// client.connect(err => {
-//     const servicesCollection = client.db("blueJazzDb").collection("allServices");
-//     console.log("mongodb connected successfully");
-
-//     // app.get('/allServices', (req, res) => {
-//     //     console.log("req called", req);
-//     //     res.send(true)
-//     // })
-
-// });
-// //mongodb << <<
-
-
-// app.listen(() => {
-//     console.log(port);
-//     console.log(`Example app listening at http://localhost:${port}`);
-// })
-
-
-
-
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectId;
 const express = require('express')
-
+const fileUpload = require('express-fileupload');
+const fs = require('fs-extra');
 
 const app = express()
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('serviceimages'));
+app.use(fileUpload());
 
 const port = process.env.PORT || 5000
 //mongodb connection
@@ -190,6 +148,56 @@ client.connect(err => {
                 res.send(doctors.length > 0);
             })
     })
+
+
+    app.post('/addServices', (req, res) => {
+        const serviceImg = req.files.file;
+        const serviceName = req.body.serviceName;
+        const serviceDescription = req.body.serviceDescription;
+        const price = req.body.price;
+        console.log(" in server ", serviceImg, serviceName, serviceDescription, price);
+        const filePath = `${__dirname}/serviceimages/${serviceImg.name}`;
+        serviceImg.mv(filePath, err => {
+            if (err) {
+                console.log(err);
+                return res.status(500).send({ msg: 'failed to upload image' })
+            }
+            const newImg = fs.readFileSync(filePath);
+            const encImg = newImg.toString();
+
+            const image = {
+                contentType: req.files.file.mimetype,
+                size: req.files.file.size,
+                img: Buffer(encImg, 'base64')
+            };
+            servicesCollection.insertOne({ serviceImg: image, serviceName, serviceDescription, price })
+                .then(result => {
+                    fs.remove(filePath, error => {
+                        if (error) {
+                            console.log(error)
+                        }
+                        res.send(result.insertedCount > 0)
+                    })
+
+                })
+            // return res.send({name: serviceImg.name, path:`/${serviceImg.name}`})
+        })
+    })
+    // app.post('/addServices', (req, res) => {
+    //     const serviceImg = req.files.file;
+    //     const serviceName = req.body.serviceName;
+    //     const serviceDescription = req.body.serviceDescription;
+    //     const price = req.body.price;
+    //     console.log(" in server ", serviceImg, serviceName, serviceDescription, price);
+
+    //     serviceImg.mv(`${__dirname}/serviceimages/${serviceImg.name}`, err => {
+    //         if(err){
+    //             console.log(err);
+    //             return res.status(500).send({msg: 'failed to upload image'})
+    //         }
+    //         return res.send({name: serviceImg.name, path:`/${serviceImg.name}`})
+    //     })
+    // })
     //code for some troubling section<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
